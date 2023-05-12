dfd6c9e8c1d1842966560e513df0f0b53a612018
ampersand8 <ampersand8@gmail.com>
2023-05-12 15:54:53 +0200

    feature: add simple REST API

---
+++ WebAPI/Controllers/MillionThingsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MillionThings;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers;
+
+[Route("api/todos")]
+[ApiController]
+public class MillionThingsController : ControllerBase
+{
+    private Todo todo;
+    public MillionThingsController()
+    {
+        todo = new JsonFileTodo("todos.json");
+
+    }
+
+    [HttpGet]
+    public IEnumerable<TodoItem> Get()
+    {
+        return todo.List();
+    }
+
+    [HttpGet("{id}")]
+    public TodoItem Get(string id)
+    {
+        return todo.List().Find(t => t.Id.Equals(id));
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string description)
+    {
+        todo.Add(description);
+    }
+
+    [HttpPut("{id}")]
+    public void Put(string id, [FromBody] string value)
+    {
+        todo.Update(new TodoItem { Id = id, Description = value });
+    }
+
+    [HttpPost("{id}/done")]
+    public void Delete(string id)
+    {
+        todo.Done(id);
+    }
+}
