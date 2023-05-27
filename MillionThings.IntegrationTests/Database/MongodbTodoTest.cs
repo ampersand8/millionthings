1416684b0cfb81665a69b5723e865f376d0171a7
ampersand8 <ampersand8@gmail.com>
2023-05-27 08:24:02 +0200

    add more mongodb todo tests

--- MillionThings.IntegrationTests/Database/MongodbTodoTest.cs
+++ MillionThings.IntegrationTests/Database/MongodbTodoTest.cs
@@ -9,22 +9,31 @@ public class MongodbTodoTest : IAsyncLifetime
     private readonly MongoDbContainer mongoContainer = new MongoDbBuilder().Build();
 
     [Fact]
+    public void ShouldBeEmptyAtStart()
+    {
+        Todo sut = new MongodbTodo(mongoContainer.GetConnectionString(), Guid.NewGuid().ToString());
+
+        Assert.Empty(sut.List());
+    }
+
+    [Fact]
     public void ShouldSaveAnAddedTask()
     {
         var testString = Guid.NewGuid().ToString();
-        Todo sut = new MongodbTodo(mongoContainer.GetConnectionString(), "todos");
+        Todo sut = new MongodbTodo(mongoContainer.GetConnectionString(), Guid.NewGuid().ToString());
         
         sut.Add(testString);
         
         Assert.Single(sut.List());
         Assert.Equal(testString, sut.List()[0].Description);
+        Assert.Equal(TodoStatus.Open, sut.List()[0].Status);
     }
 
     [Fact]
     public void ShouldMarkAFinishedTaskAsDone()
     {
         var testString = Guid.NewGuid().ToString();
-        Todo sut = new MongodbTodo(mongoContainer.GetConnectionString(), "todos");
+        Todo sut = new MongodbTodo(mongoContainer.GetConnectionString(), Guid.NewGuid().ToString());
 
         sut.Add(testString);
         var taskToMarkAsFinished = sut.List().Find(t => t.Description == testString);
@@ -32,6 +41,19 @@ public class MongodbTodoTest : IAsyncLifetime
 
         var result = sut.List().Find(t => t.Description == testString);
         Assert.Equal(TodoStatus.Done, result.Status);
+    }
+
+    [Fact]
+    public void ShouldNotListRemovedTasks()
+    {
+        var testString = Guid.NewGuid().ToString();
+        Todo sut = new MongodbTodo(mongoContainer.GetConnectionString(), Guid.NewGuid().ToString());
+
+        sut.Add(testString);
+        var taskToBeDeleted = sut.List().Find(t => t.Description == testString);
+        sut.Delete(taskToBeDeleted.Id);
+
+        Assert.Empty(sut.List());
     }
 
     public Task InitializeAsync()
