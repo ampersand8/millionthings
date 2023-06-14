0dd596bb13be7d2257b7c51973bb6ffdf9b2ff73
ampersand8 <ampersand8@gmail.com>
2023-06-14 13:34:57 +0200

    add some more testing

--- MillionThings.Tests/SimpleWebApp/IndexPagesTest.cs
+++ MillionThings.Tests/SimpleWebApp/IndexPagesTest.cs
@@ -20,19 +20,38 @@ public class IndexPagesTest
         Assert.Single(sut.Tasks);
     }
 
+    [Fact]
+    public void ShouldRemoveATaskWhenClickingDelete()
+    {
+        var sut = new IndexModel(NullLogger<IndexModel>.Instance, Guid.NewGuid() + ".json");
+
+        sut.PageContext = CreateContext(new() { { "todo", sut.CurrentTodoId } });
+
+        sut.OnPost("this is a test");
+        Assert.Single(sut.Tasks);
+
+        sut.PageContext = CreateContext(new()
+            { { "todo", sut.CurrentTodoId }, { "id", sut.Tasks[0].Id }, { "action", "delete" } });
+        sut.OnGet();
+        Assert.Empty(sut.Tasks);
+    }
+
     private PageContext CreateContext(Dictionary<string, StringValues> queryParams)
     {
         var httpContextMock = new Mock<HttpContext>();
         var httpRequestMock = new Mock<HttpRequest>();
+        var httpResponseMock = new Mock<HttpResponse>();
 
         // Set up the QueryString and Query properties
         httpRequestMock.Setup(r => r.QueryString)
             .Returns(new QueryString("?" + string.Join("&", queryParams.Select(kv => kv.Key + "=" + kv.Value))));
         httpRequestMock.Setup(r => r.Query)
             .Returns(new QueryCollection(queryParams));
+        
 
         // Assign the mocked HttpRequest to the mocked HttpContext
         httpContextMock.Setup(c => c.Request).Returns(httpRequestMock.Object);
+        httpContextMock.Setup(c => c.Response).Returns(httpResponseMock.Object);
 
         return new PageContext
         {
